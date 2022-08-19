6d9fd09a1fc1bf6372601ccc3eb07184684d7a20
Martirena <luis.martirena@thermofisher.com>
2022-08-18 19:44:23 -0600

    totalProductos

--- dashboard/src/components/TotalProductos.js
+++ dashboard/src/components/TotalProductos.js
@@ -4,30 +4,30 @@ import { useState, useEffect } from 'react'
 function TotalProductos(){
     const [productos, setProductos] = useState([])
 
+    useEffect(() => {
+        console.log("%cSe monto el componente", "color: green");
+        fetch('http://localhost:3000/api/products')
+        .then(response => response.json())
+        .then(data => {
+            setProductos(data.productos)
+            console.log("Total de Productos: ", data);
+        })
+        .catch(err => console.log(err));
+    }, [])
     // useEffect(() => {
     //     console.log("%cSe monto el componente", "color: green");
-    //     fetch('http://localhost:3000/api/products')
+    //     fetch('http://localhost:3000/api/products', {
+    //         headers: {
+    //             authorization: ' xxxxxxxxxx' ,
+    //             'Content-Type': 'application/json'
+    //          }
+    //     })
     //     .then(response => response.json())
     //     .then(data => {
     //         setProductos(data)
-    //         console.log("Total de Productos: ", data);
     //     })
     //     .catch(err => console.log(err));
-    // }, [])
-    useEffect(() => {
-        console.log("%cSe monto el componente", "color: green");
-        fetch('http://localhost:3000/api/products', {
-            headers: {
-                authorization: ' xxxxxxxxxx' ,
-                'Content-Type': 'application/json'
-             }
-        })
-        .then(response => response.json())
-        .then(data => {
-            setProductos(data)
-        })
-        .catch(err => console.log(err));
-    }, []) 
+    // }, []) 
 
     
     return(
